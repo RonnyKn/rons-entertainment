5f47b98c6d49b7f6222d551c40fbfb316800a5eb
RonnyKn <ronnykurniawn@gmail.com>
2022-09-16 14:37:09 +0700

    update UI Message me feature with Material-UI

--- src/pages/Message/Message.js
+++ src/pages/Message/Message.js
@@ -1,6 +1,8 @@
 import './Message.css'
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { Button, createTheme, TextareaAutosize, TextField, ThemeProvider } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send'
 
 const Message = () => {
 
@@ -12,33 +14,73 @@ const Message = () => {
 
     emailjs.sendForm('service_2y24t5g', 'template_2gpy8ly', form.current, '8sH3i4bVXXbpdpkfh')
       .then((result) => {
-        console.log(result.text);
+        // console.log(result.text);
         setDone(true)
       }, (error) => {
         console.log(error.text);
       });
   };
   if (isDone === true) {
-    window.alert("Thankyou for your feedback. \nHave a nice day ❤.");
+    window.alert("Thankyou for your feedback. \nHope you have a great day ❤.");
     window.location.reload();
-
   }
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#fff'
+      },
+      secondary: {
+        main: '#293349'
+      }
+    }
+  })
 
   return (
     <>
       <span className='page-title'>Message Me..</span>
-      <p className='page-subTitle'><em>..if you're like this website or some ideas/features that can improve website.❤ </em></p>
-      <div className='message'>
-        <form ref={form} className='message-form' onSubmit={sendEmail}>
-          <label>Name :</label>
-          <input type="text" name='user_name' placeholder='ex: Cahyonoo' className='user' id='user_name' required />
-          <label>E-mail:</label>
-          <input type="email" name='user_email' placeholder='Cahyono@gmail.com' className='user' required ></input>
-          <label >Message:</label>
-          <textarea name="message" id="" cols="30" rows="10" placeholder='message..' className='user' required ></textarea>
-          <input type="submit" value="Send" className='button send-button' />
-        </form>
-      </div>
+      <p className='page-subTitle'><em>..if you're have some ideas or features that can improve website.❤ </em></p>
+      <ThemeProvider theme={darkTheme}>
+        <div className='message'>
+          <form ref={form} className='message-form' onSubmit={sendEmail}>
+            <TextField
+              type='text'
+              name='user_name'
+              placeholder='ex:Aliandoo'
+              className='user'
+              id='user_name'
+              label='Name'
+              required
+            />
+            <TextField
+              type='email'
+              name='user_email'
+              placeholder='Aliandoo@gmail.com'
+              className='user'
+              label='E-mail'
+              required
+            />
+            <TextareaAutosize
+              aria-label="maximum height"
+              name='message'
+              placeholder='tell me something about this website?'
+              className='user'
+              minRows={3}
+              required
+              style={{ width: 400, padding: '5px', borderRadius: '5px' }}
+            />
+            <Button
+              variant='contained'
+              type='submit'
+              color='secondary'
+              style={{ margin: '20px auto', width: '150px', border: "1px solid white" }}
+            >
+              Send <SendIcon />
+            </Button>
+          </form>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
