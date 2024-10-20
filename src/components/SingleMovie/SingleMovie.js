00f16a6b56174bff8de5cb27a95ba1ad4e1e7cc6
ronnykn <ronnykurniawn@gmail.com>
2024-10-20 15:26:20 +0700

    fix bug

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,45 +1,68 @@
-import './SingleMovie.css'
-import React, { useEffect, useState } from 'react'
-import { img_300, img_500, unavailable } from '../../config/Config'
-import { Badge } from '@mui/material'
-import { Button, Modal } from 'react-bootstrap'
-import YtIcon from '@mui/icons-material/YouTube';
-import CloseIcon from '@mui/icons-material/ExitToApp';
-import axios from 'axios'
+import "./SingleMovie.css";
+import React, { useEffect, useState } from "react";
+import { img_300, img_500, unavailable } from "../../config/Config";
+import { Badge } from "@mui/material";
+import { Button, Modal } from "react-bootstrap";
+import YtIcon from "@mui/icons-material/YouTube";
+import CloseIcon from "@mui/icons-material/ExitToApp";
+import axios from "axios";
 
-const SingleMovie = ({ id, poster_path, title, date, media_type, vote_average, overview }) => {
-
+const SingleMovie = ({
+  id,
+  poster_path,
+  title,
+  date,
+  media_type,
+  vote_average,
+  overview,
+}) => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const [video, setVideo] = useState()
-  const [video2, setVideo2] = useState()
-  const [video3, setVideo3] = useState()
+  const [video, setVideo] = useState();
+  const [video2, setVideo2] = useState();
+  const [video3, setVideo3] = useState();
 
   const fetchVideo = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`
-    )
-    setVideo(data.results[0]?.key)
-    setVideo2(data.results[1]?.key)
-    setVideo3(data.results[2]?.key)
-  }
+    );
+    setVideo(data.results[0]?.key);
+    setVideo2(data.results[1]?.key);
+    setVideo3(data.results[2]?.key);
+  };
 
   useEffect(() => {
-    fetchVideo()
+    fetchVideo();
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   return (
     <>
-      <div className='media' onClick={handleOpen} >
-        <Badge badgeContent={vote_average.toFixed(1)} color={vote_average >= 6 ? "primary" : "secondary"} />
-        <div className='poster-div' >
-          <img src={poster_path ? `${img_300}${poster_path}` : unavailable} alt={title} />
+      <div className="media" onClick={handleOpen}>
+        <Badge
+          badgeContent={vote_average?.toFixed(1)}
+          color={vote_average >= 6 ? "primary" : "secondary"}
+        />
+        <div className="poster-div">
+          <img
+            src={poster_path ? `${img_300}${poster_path}` : unavailable}
+            alt={title}
+          />
         </div>
-        <h5 style={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }} className='title'>{title}</h5>
-        <span className='subtitle'>{media_type === "tv" ? "TV Series" : "Movie"}
-          <span >{date}</span>
+        <h5
+          style={{
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+          }}
+          className="title"
+        >
+          {title}
+        </h5>
+        <span className="subtitle">
+          {media_type === "tv" ? "TV Series" : "Movie"}
+          <span>{date}</span>
         </span>
       </div>
 
@@ -47,45 +70,107 @@ const SingleMovie = ({ id, poster_path, title, date, media_type, vote_average, o
         show={open}
         onHide={handleClose}
         className="modal"
-        size='xl'
-        style={{ backgroundColor: 'transparent' }}
+        size="xl"
+        style={{ backgroundColor: "transparent" }}
       >
         <Modal.Header>
-          <Modal.Title className='m-title' style={{ fontFamily: 'Gumela', fontSize: '2rem' }} > <strong> Movie Details </strong></Modal.Title>
+          <Modal.Title
+            className="m-title"
+            style={{ fontFamily: "Gumela", fontSize: "2rem" }}
+          >
+            {" "}
+            <strong> Movie Details </strong>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="modal-content">
-            <div className='modal-box'>
-              <div className="ribbon"><span>Rating: {vote_average.toFixed(1)}</span></div>
-              <img src={poster_path ? img_500 + poster_path : unavailable} alt="" className="modal-image" />
+            <div className="modal-box">
+              <div className="ribbon">
+                <span>Rating: {vote_average?.toFixed(1)}</span>
+              </div>
+              <img
+                src={poster_path ? img_500 + poster_path : unavailable}
+                alt=""
+                className="modal-image"
+              />
               <div className="modal-body">
-                <p className='title-1'><strong>Title : <br /></strong>{title}</p>
-                <h3 className='title-2' style={{ fontFamily: 'Gumela', textAlign: 'center' }}><strong><em>{title}</em></strong></h3>
-                <p><strong>Release : <br /></strong>{date}</p>
-                <p><strong>Overview : <br /></strong>{overview}</p>
+                <p className="title-1">
+                  <strong>
+                    Title : <br />
+                  </strong>
+                  {title}
+                </p>
+                <h3
+                  className="title-2"
+                  style={{ fontFamily: "Gumela", textAlign: "center" }}
+                >
+                  <strong>
+                    <em>{title}</em>
+                  </strong>
+                </h3>
+                <p>
+                  <strong>
+                    Release : <br />
+                  </strong>
+                  {date}
+                </p>
+                <p>
+                  <strong>
+                    Overview : <br />
+                  </strong>
+                  {overview}
+                </p>
               </div>
             </div>
           </div>
         </Modal.Body>
         <Modal.Footer>
-          {video === undefined ? <span></span> :
-            <div className='trailers'>
-              <Button className='btnTrailer' variant='danger' target='__blank'
-                href={`https://www.youtube.com/watch?v=${video}`}> <YtIcon /> <strong className='strong-trailer'> Trailer 1</strong>
+          {video === undefined ? (
+            <span></span>
+          ) : (
+            <div className="trailers">
+              <Button
+                className="btnTrailer"
+                variant="danger"
+                target="__blank"
+                href={`https://www.youtube.com/watch?v=${video}`}
+              >
+                {" "}
+                <YtIcon />{" "}
+                <strong className="strong-trailer"> Trailer 1</strong>
               </Button>
-              <Button className='btnTrailer' variant='danger' target='__blank'
-                href={`https://www.youtube.com/watch?v=${video2}`}> <YtIcon /> <strong className='strong-trailer'> Trailer 2</strong>
+              <Button
+                className="btnTrailer"
+                variant="danger"
+                target="__blank"
+                href={`https://www.youtube.com/watch?v=${video2}`}
+              >
+                {" "}
+                <YtIcon />{" "}
+                <strong className="strong-trailer"> Trailer 2</strong>
               </Button>
-              <Button className='btnTrailer' variant='danger' target='__blank'
-                href={`https://www.youtube.com/watch?v=${video3}`}> <YtIcon /> <strong className='strong-trailer'> Trailer 3</strong>
+              <Button
+                className="btnTrailer"
+                variant="danger"
+                target="__blank"
+                href={`https://www.youtube.com/watch?v=${video3}`}
+              >
+                {" "}
+                <YtIcon />{" "}
+                <strong className="strong-trailer"> Trailer 3</strong>
               </Button>
             </div>
-          }
-          <Button className='close' variant='secondary' onClick={handleClose}><strong> Close <CloseIcon /> </strong></Button>
+          )}
+          <Button className="close" variant="secondary" onClick={handleClose}>
+            <strong>
+              {" "}
+              Close <CloseIcon />{" "}
+            </strong>
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default SingleMovie
+export default SingleMovie;
